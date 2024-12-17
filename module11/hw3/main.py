3e71cc93075d4a50677fe306b1104b5634e0df39
Viktor <80nph1r3@gmail.com>
2024-12-17 21:41:10 +0300

    Added module11/hw3

---
+++ module11/hw3/main.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import inspect
+import random
+import math
+
+
+def introspection_info(obj: object) -> dict:
+    module = inspect.getmodule(obj)
+    attrs = {
+        "type": type(obj).__name__,
+        "attributes": [],
+        "methods": [],
+        "module": module.__name__ if module else None,
+    }
+    for name, value in inspect.getmembers(obj):
+        attrs["attributes"].append(name)
+        if callable(value):
+            attrs["methods"].append(name)
+    return attrs
+
+
+if __name__ == "__main__":
+    pprint(introspection_info(random.choice))
+    pprint(introspection_info(math.pi))
+    pprint(introspection_info(42))
