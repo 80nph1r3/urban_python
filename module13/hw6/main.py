98e45d1e2a37da50d26ffed3591398597d54b7e3
Viktor <80nph1r3@gmail.com>
2024-12-18 15:20:22 +0300

    Added module13/hw6

---
+++ module13/hw6/main.py
@@ -0,0 +1,100 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    CallbackQuery,
+)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import CommandStart
+
+API_TOKEN = ""
+FORM_ROUTER = Router()
+
+
+class UserState(StatesGroup):
+    age = State()
+    height = State()
+    weight = State()
+
+
+@FORM_ROUTER.message(CommandStart())
+async def start(message: Message) -> None:
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Рассчитать"), KeyboardButton(text="Информация")]
+        ],
+        resize_keyboard=True,
+    )
+    await message.answer(
+        "Привет! Я бот, помогающий твоему здоровью.", reply_markup=keyboard
+    )
+
+
+@FORM_ROUTER.message(F.text == "Рассчитать")
+async def main_menu(message: Message) -> None:
+    inline_keyboard = InlineKeyboardBuilder(
+        [
+            [
+                InlineKeyboardButton(
+                    text="Рассчитать норму калорий", callback_data="calories"
+                ),
+                InlineKeyboardButton(text="Формулы расчёта", callback_data="formulas"),
+            ]
+        ]
+    )
+    await message.answer("Выберите опцию", reply_markup=inline_keyboard.as_markup())
+
+
+@FORM_ROUTER.callback_query(F.data == "formulas")
+async def send_formula(call: CallbackQuery) -> None:
+    await call.message.answer("10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5")
+
+
+@FORM_ROUTER.callback_query(F.data == "calories")
+async def set_age(call: CallbackQuery, state: FSMContext) -> None:
+    await call.message.answer("Введите свой возраст")
+    await state.set_state(UserState.age)
+
+
+@FORM_ROUTER.message(UserState.age)
+async def set_height(message: Message, state: FSMContext) -> None:
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост")
+    await state.set_state(UserState.height)
+
+
+@FORM_ROUTER.message(UserState.height)
+async def set_weight(message: Message, state: FSMContext) -> None:
+    await state.update_data(height=message.text)
+    await message.answer("Введите свой вес")
+    await state.set_state(UserState.weight)
+
+
+@FORM_ROUTER.message(UserState.weight)
+async def send_calories(message: Message, state: FSMContext) -> None:
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = (
+        int(data["weight"]) * 10 + int(data["height"]) * 6.25 - int(data["age"]) * 5 + 5
+    )
+    await message.answer(f"Норма калорий: {result}")
+
+
+async def main() -> None:
+    bot = Bot(token=API_TOKEN)
+
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(FORM_ROUTER)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
